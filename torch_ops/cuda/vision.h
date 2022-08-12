bedf0d701acca1859093112151ffe352e9d38369
Haoyi Zhu <zhuhaoyi@sjtu.edu.cn>
2022-08-12 22:28:05 +0800

    support for pytorch >= 1.11

--- torch_ops/cuda/vision.h
+++ torch_ops/cuda/vision.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <torch/extension.h>
-#include <THC/THC.h>
+#include <ATen/cuda/CUDAEvent.h>
 
 void knn_device(float* ref_dev, int ref_width,
     float* query_dev, int query_width,
